8d53a4723723e797ccc0f5bc678fca7411891bd4
James Ferguson <ncjamesferguson@gmail.com>
2017-12-19 19:55:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    self.a = a
+    self.b = b
+    self.c = c
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+    if self.a <= 0 || self.b <= 0 || self.c <= 0 || self.a + self.b < self.c || self.a + self.c < self.b || self.b + self.c < self.a
+      raise TriangleError
+    elsif self.a == self.b && self.b == self.c
+      :equilateral
+    elsif self.a == self.b || self.b == self.c || self.a == self.c
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
